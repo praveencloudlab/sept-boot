7380f5175bc7b72ffff3391ba17eaa6342a53c17
Praveenreddy <praveen@Praveenreddys-MacBook-Pro.local>
2023-09-20 16:29:21 +0530

    boot app added

---
+++ boot-ws/1-boot-basics/src/main/java/com/cts/ecart/C1.java
@@ -0,0 +1,16 @@
+package com.cts.ecart;
+
+import org.springframework.stereotype.Component;
+
+//@Service
+//@Repository
+//@Controller
+//@Configuration
+@Component
+public class C1 {
+	
+	public C1() {
+		System.out.println(">>C1 class object created..");
+	}
+
+}
---
+++ boot-ws/1-boot-basics/src/main/java/com/cts/ecart/dao/urils/C5.java
@@ -0,0 +1,12 @@
+package com.cts.ecart.dao.urils;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class C5 {
+	
+	public C5() {
+		System.out.println(">>C5 class object created..");
+	}
+
+}
